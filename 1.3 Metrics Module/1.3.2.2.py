d939723573e2adc56b43033e3bc6b3ee9c67e290
Nourasha <69981085+Nourasha@users.noreply.github.com>
2024-10-21 10:48:29 +0200

    Initial commit

---
+++ 1.3 Metrics Module/1.3.2.2.py
@@ -0,0 +1,17 @@
+
+from sklearn.metrics import mean_squared_error
+y_true = [3, -0.5, 2, 7]
+y_pred = [2.5, 0.0, 2, 8]
+mean_squared_error(y_true, y_pred)
+
+y_true = [[0.5, 1],[-1, 1],[7, -6]]
+y_pred = [[0, 2],[-1, 2],[8, -5]]
+
+
+life = mean_squared_error(y_true, y_pred)
+print(life)
+
+mean_squared_error(y_true, y_pred, multioutput='uniform_average') 
+
+
+mean_squared_error(y_true, y_pred, multioutput='raw_values')
---
+++ 1.3 Metrics Module/1.3.7.2.py
@@ -0,0 +1,5 @@
+from sklearn.metrics import recall_score
+y_pred =  ['a','b','c','a','b','c','a','b','c','a']
+y_true =   ['a','a','b','b','a','b','c','c','b','b']
+ms=recall_score(y_true, y_pred, average=None)
+print(ms)
